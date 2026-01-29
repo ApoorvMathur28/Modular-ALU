63a4250092eb6cce3831e6b80ccb18d2874f4622
ApoorvMathur28 <apoorvmathur289@gmail.com>
2026-01-29 14:14:08 +0530

    Delete div.v

--- div.v
+++
@@ -1,8 +0,0 @@
-module div(
-    input  [3:0] A, B,
-    output [3:0] QUOT,
-    output [3:0] REM
-);
-    assign QUOT = (B != 0) ? (A / B) : 4'b0000;
-    assign REM  = (B != 0) ? (A % B) : 4'b0000;
-endmodule
