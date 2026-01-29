70144dfb0a7a3f6fc2ddc11d9485b62ede034ece
ApoorvMathur28 <apoorvmathur289@gmail.com>
2026-01-29 14:13:58 +0530

    Delete comparator.v

--- comparator.v
+++
@@ -1,9 +0,0 @@
-module compare(
-    input  [3:0] A,
-    input  [3:0] B,
-    output [2:0] CMP
-);
-    assign CMP[0] = (A == B); // Equal flag
-    assign CMP[1] = (A >  B); // Greater flag
-    assign CMP[2] = (A <  B); // Lesser flag
-endmodule
